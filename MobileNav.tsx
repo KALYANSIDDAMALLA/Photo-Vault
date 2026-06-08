2ce9b7b309e007cd2892c56cb09ad96997d1e7b1
Your Name <you@example.com>
2026-06-09 00:22:58 +0530

    remove emojis from nav

--- MobileNav.tsx
+++ MobileNav.tsx
@@ -4,12 +4,28 @@ import Link from "next/link";
 
 export default function MobileNav() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 h-20 bg-red-500 flex justify-around items-center z-[99999]">
-      <Link href="/explore">🏠</Link>
-      <Link href="/trending">🔥</Link>
-      <Link href="/photoupload">➕</Link>
-      <Link href="/search">🔍</Link>
-      <Link href="/profile">👤</Link>
+    <div className="fixed bottom-0 left-0 right-0 h-16 bg-black border-t border-gray-800 flex justify-around items-center z-[9999]">
+
+      <Link href="/explore" className="text-white">
+        Home
+      </Link>
+
+      <Link href="/trending" className="text-white">
+        Trending
+      </Link>
+
+      <Link href="/photoupload" className="text-white">
+        Upload
+      </Link>
+
+      <Link href="/search" className="text-white">
+        Search
+      </Link>
+
+      <Link href="/profile" className="text-white">
+        Profile
+      </Link>
+
     </div>
   );
 }
