b7adb16a89d7cf8c1b9529fd90d236933ca4eb49
Your Name <you@example.com>
2026-06-09 00:06:18 +0530

    fix mobile nav

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -4,26 +4,26 @@ import Link from "next/link";
 
 export default function MobileNav() {
   return (
-    <div className="fixed bottom-0 left-0 right-0 bg-black border-t border-gray-800 flex justify-around items-center h-16 z-50">
+    <div className="fixed bottom-0 left-0 right-0 h-16 bg-black border-t border-gray-800 flex justify-around items-center z-[9999]">
 
-      <Link href="/explore">
+      <Link href="/explore" className="text-white text-2xl">
         🏠
       </Link>
 
-      <Link href="/trending">
+      <Link href="/trending" className="text-white text-2xl">
         🔥
       </Link>
 
-      <Link href="/photoupload">
+      <Link href="/photoupload" className="text-white text-2xl">
         ➕
       </Link>
 
-      <Link href="/search">
+      <Link href="/search" className="text-white text-2xl">
         🔍
       </Link>
 
-      <Link href="/profile">
-        👤
+      <Link href="/profile" className="text-white text-2xl">
+        ��
       </Link>
 
     </div>
