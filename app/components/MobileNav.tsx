269c43f40521e80e2a4e0dbca19e9b4b8f4cc825
Your Name <you@example.com>
2026-06-09 00:13:38 +0530

    fix profile nav icon

--- app/components/MobileNav.tsx
+++ app/components/MobileNav.tsx
@@ -6,24 +6,24 @@ export default function MobileNav() {
   return (
     <div className="fixed bottom-0 left-0 right-0 h-16 bg-black border-t border-gray-800 flex justify-around items-center z-[9999]">
 
-      <Link href="/explore" className="text-white text-2xl">
-        🏠
+      <Link href="/explore" className="flex flex-col items-center text-white">
+        <span className="text-xl">🏠</span>
       </Link>
 
-      <Link href="/trending" className="text-white text-2xl">
-        🔥
+      <Link href="/trending" className="flex flex-col items-center text-white">
+        <span className="text-xl">🔥</span>
       </Link>
 
-      <Link href="/photoupload" className="text-white text-2xl">
-        ➕
+      <Link href="/photoupload" className="flex flex-col items-center text-white">
+        <span className="text-xl">➕</span>
       </Link>
 
-      <Link href="/search" className="text-white text-2xl">
-        🔍
+      <Link href="/search" className="flex flex-col items-center text-white">
+        <span className="text-xl">🔍</span>
       </Link>
 
-      <Link href="/profile" className="text-white text-2xl">
-        ��
+      <Link href="/profile" className="flex flex-col items-center text-white">
+        <span className="text-xl">🙂</span>
       </Link>
 
     </div>
